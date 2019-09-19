912fe8129f5f8987acdc26317ae477f84ab1bb9e
nallumartins <contatonathaliamartins@gmail.com>
2019-09-19 12:08:43 -0300

    colocar herança

--- aula3.py
+++ aula3.py
@@ -10,8 +10,23 @@ class Receita(): # class é a palavra para criar o objeto Receita é o nome do o
         self.porcao = porcao
 
     def get_all(self): # com o get nós retornamos o que colocamos
-        print("Para fazer está receita, é preciso de {}. Os utensílios necessários são {}. Para fazer a receita {}. Essa receita rende {}" .format(self.ingredientes, self.utensilios,self.preparo, self.porcao))
+        print(" Ingredientes: {}\n Utensílios: {}\n Preparo:{}\n Rendimento: {}\n" .format(self.ingredientes, self.utensilios,self.preparo, self.porcao))
 
-# aqui estou atribuindo os métodos da minha receita
-comida = Receita("1 xícara de farinha, 1 ovo, azeite e sal a gosto", "tigela, garfo, colher, espátulo e frigideira", "coloque o ovo e leite na tigela, acrescente a farinha aos poucos para não empelotar. Coloco a frigideira no fogo, unte com azeite, coloque 2 colheres de sopa da massa e vire quando a massa estiver com as bordas douradas, deixe mais 2 minutos e retire" , "6 porções.")
+# aqui estou instanciando, colocando atributos aos métodos
+comida = Receita("1 xícara de farinha, 1 ovo, azeite e sal a gosto", "tigela, garfo, colher, espátulo e frigideira", " mistura tudo e coloca na frigideira" , "6 porções.")
 comida.get_all()
+
+class Doce(Receita):
+
+    def __init__(self, ingredientes, utensilios, preparo, porcao):
+        self.ingredientes = ingredientes
+        self.utensilios = utensilios
+        self.preparo = preparo
+        self.porcao = porcao
+
+    def brigadeiro(self):
+        print (" Ingredientes: {}\n Utensílios: {}\n Preparo: {}\n Porção: {}\n" .format(self.ingredientes, self.utensilios, self. preparo, self.porcao))
+
+docinho = Doce("Leite Condensado e chocolate", "panela e colher", "colocar ingredientes na panela e misturar", "30 brigadeiros")
+docinho.brigadeiro()    
+    
